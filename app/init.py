202c1eb6d335eb95cb183af8b846300acc9bbee6
Rafael de Oliveira Alves <167444291+babuinodev@users.noreply.github.com>
2025-01-31 22:35:21 -0300

    __init__.py

--- app/init.py
+++ app/init.py
@@ -5,7 +5,7 @@ def create_app():
     app.secret_key = "beeb729ffffb25428e39687d507ea476f105a5ab523b1db753f0830df3893cf4"
 
     # Registrar blueprints
-    from app.routes import main_bp # type: ignore
+    from app.routes import main_bp
     app.register_blueprint(main_bp)
 
     return app
